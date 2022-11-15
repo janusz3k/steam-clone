5b4658600d3e627a71547c7a7e8907279481b942
Hubert Januszek <hubert.januszek.kielce@gmail.com>
2022-11-15 18:09:57 +0100

    Initial project structure

---
+++ src/index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Zaloguj się</title>
+
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div id="global_header">
+        <div id="menu">
+            <img style="height: 100px;" src="../img/logo.png" alt="Logo"/>
+        </div>
+    </div>
+	<div id="page_content">
+	    <h1>Zaloguj się</h1>
+        <div class="login">
+
+        </div>
+	</div>
+	<div id="footer">
+
+    </div>
+</body>
+</html>
